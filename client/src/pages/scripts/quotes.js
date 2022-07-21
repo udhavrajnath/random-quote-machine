e518e3a388986464ebf8bb0ac004d21b4ac47041
udhavrajnath <70680597+udhavrajnath@users.noreply.github.com>
2022-07-22 01:36:25 +1000

    Generate the quotes based on random number between 0 and 16

--- client/src/pages/scripts/quotes.js
+++ client/src/pages/scripts/quotes.js
@@ -6,33 +6,45 @@ import { useEffect, useState } from "react";
 function Quotes() {
 
 const [quotesArray, setQuote]=useState([]) 
+const [displayquote,setdisplayedQuote]=useState("")
+const [displayauthor,setdisplayedAuthor]=useState("")
 const quotesAPI_URL="/quoteAPI";
 
 function generatequotes(){
     console.log("button is working");
+    //generate a random number betwwen 0-16 that will be passed as index in the the quote state array
+    let myindex = Math.floor((Math.random()*17));
+    console.log(myindex);
+    if(quotesArray.length>0){
+        setdisplayedQuote(quotesArray[myindex].quote);
+        setdisplayedAuthor(quotesArray[myindex].name);
+    }
+
 }
 
 function FetchQuotes(){
-    fetch('/quoteAPI')
+    fetch('http://localhost:3001/quoteAPI')
     .then(res=>{
         return res.json()
     })
     .then((response)=>{
         console.log(response);
+        setQuote(response.quoteslist);
+        console.log("this is the quote state now", quotesArray);
     })
    
 }
 useEffect(()=>{
  FetchQuotes();   
-})
+},[quotesAPI_URL])
 
   return (
     <div className="quotepage">
         <div className="mainmiddlebox">
-            <div className="pagetitle"><h1>Random Quote Generator</h1></div>
+            <div className="pagetitle"><h1 id="pagetitle">Random Quote Generator</h1></div>
             <div className="quote">
                 <div className="quotetext">
-                <h4>blaa</h4>
+                <h2 id="quotetextdesc">"{displayquote}" </h2><br/><h4 id="quoteauthordesc">~ <strong>{displayauthor}</strong></h4>
                 </div>
                 <div className="quotebtn">
                     <button id="nextbtn" onClick={()=>generatequotes()}>Generate</button>
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express =require('express');
 const app=express();
 const mysql= require('mysql');
+const cors = require('cors');
 
 const db =mysql.createConnection({
     user:"testuser",
@@ -13,18 +14,23 @@ const db =mysql.createConnection({
 app.listen(3001, ()=>{
     console.log("You are listening on port 3001");
 })
+app.use(express.json());
+app.use(cors());
 
-app.get("/quoteAPI",(req,res)=>{
-    query1 = "SELECT * FROM randomquoteslist. randomquotes";
+
+app.get(`/quoteAPI/`,(req,res)=>{
+    query1 = "SELECT * FROM randomquoteslist.randomquotes"
     const quoteslist=[];
+    
     db.query(query1,(err,result)=>{
-        //create a quote object from the data pulled from database that will then be pushed to the quoteslist and sent to the API
-        quote={};
-        for(var i=0; i<result.length;i++){
-            quote["name"]=result[i].name;
-            quote["quote"]=result[i].quote;
+        //create a quote object from the data pulled from database that will then be pushed to the quoteslist and sent to the API  
+        for(var i=0; i<result.length; i++){
+            quote={};
+            quote["name"]=result[i].name
+            quote["quote"]=result[i].quote
             quoteslist.push(quote);
         }
+        console.log(quoteslist);
         res.send({quoteslist});
     })
     
